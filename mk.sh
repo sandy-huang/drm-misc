74a75d28dc635a8935e77f33d2486ce013f376a7
Sandy Huang <hjc@rock-chips.com>
2020-06-19 16:15:19 +0800

    update mk.sh
    Signed-off-by: Sandy Huang <hjc@rock-chips.com>

--- mk.sh
+++ mk.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-#export CROSS_COMPILE=../prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-#make rockchip_defconfig -j8 
 
-if [ $1 = 3399 ];then
+if [ $1 = 3399 ];
+then
    echo "compile rk3399"
    export ARCH=arm64
    export CROSS_COMPILE=/home/hjc/prebuilts/gcc/linux-x86/aarch64/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
-   
-elif [ $1 = 3288 ];then
+elif [ $1 = 3288 ];
+then
     echo "compile rk3288"
 fi
 
-if [ $2 = menuconfig ];then
+if [ $2 = menuconfig ];
+then
     make menuconfig
-elif [ $2 = savedefconfig ];then
+elif [ $2 = savedefconfig ];
+then
     make savedefconfig
 else
     cp ~/work/develop/upstream/buildroot/output/images/rootfs.cpio.lz4 ./
@@ -22,7 +23,6 @@ else
     make Image -j8
     make dtbs -j8
 fi
-#make ARCH=arm64 CROSS_COMPILE=/home/hjc/prebuilts/gcc/linux-x86/aarch64/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 
 #./resource_tool  arch/arm64/boot/dts/rockchip/$1.dtb
 #./mkkrnlimg arch/arm64/boot/Image kernel.img
